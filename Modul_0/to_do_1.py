4e0fa317f059f2e6350fe6827c07c86bd7975ab9
Martin Hronský <hronsky4@stud.uniza.sk>
2026-02-18 11:00:24 +0100

    init TO-DO 1

---
+++ Modul_0/to_do_1.py
@@ -0,0 +1,55 @@
+print("=== Bufetový poradca ===")
+print("Vyberie ti jedlo podľa času, rozpočtu a chuti.\n")
+
+minutes_str = input("Koľko minút máš do hodiny? (napr. 12): ").strip()
+budget_str = input("Koľko € máš? (napr. 3): ").strip()
+mood_raw = input("Chuť? (SLADKE/SLANE): ")
+
+"""
+TO-DO 1
+Normalizuj mood: odstráň okrajové medzery a zmeň na veľké písmená
+mood = ...
+"""
+
+mood = "TODO"
+
+minutes = 10
+budget = 2
+
+"""
+TO-DO 2:
+Bezpečný casting:
+- if minutes_str je číslo → minutes = minutes_str typu Integer
+- if budget_str je číslo → budget = budget_str typu Integer
+(inak nechaj defaulty)
+
+if ...:
+    ...
+if ...:
+    ...
+"""
+
+print("\nDEBUG:", "minutes =", minutes, "| budget =", budget, "| mood =", mood)
+
+"""
+# TO-DO 3:
+# Rozhodovanie (nastav premennú recommendation):
+# - if minutes <= 5 → "banán alebo tyčinka"
+# - elif budget <= 2 → "rožok + šunka"
+# - else:
+#     - if mood == "SLADKE" → "koláč + čaj"
+#     - elif mood == "SLANE" → "sendvič"
+#     - else → "voda"
+"""
+
+recommendation = "TODO"
+
+print("\nOdporúčanie:", recommendation)
+
+"""
+# Bonus: 
+# if budget >= 5 vypíš "Môžeš si dať aj niečo extra"
+# 
+# if ...:
+#     ...
+"""
